6736abd80f27ac06c5d6416300ce9456d8471a0e
Keith Miller <36780654+Keimille@users.noreply.github.com>
2018-04-09 20:35:37 -0400

    Create storingVariables.c

---
+++ storingVariables.c
@@ -0,0 +1,44 @@
+/*
+* File:firstcode.c
+* Author: Keith
+*
+* Created on 9 April, 2018
+*/
+
+#include <stdio.h>
+
+
+/*
+*
+*
+*/
+
+int main () {
+    //input and store values
+    //declaration types
+    int aVar;
+    aVar = 5;
+    float x;
+    x = 6;
+    double d;
+    d = 12; //you can only use a variable after th edeclaration
+   //name of variable should not include space or special characters
+   //cannot declare a variable with a digit example: int 1x;
+   scanf(--,)//scanf allows the system the abilty to enter the text character (does not have to be static)
+   scanf("%d")//cheanges text into into signed decimal integer
+   scanf("%d")//unsigned integer
+}
+int main(){
+    int aVar;
+    scanf("%d", &aVar);//scanf we use the operating system to execute a decimal interger and then assign it ot aVar
+    printf("The value is :%d\n", aVar);
+}//run this last block of code in CodeBlock to test
+
+int main(){
+   int aVar, x;
+   printf("Enter an integer:");
+   scanf("%d", &aVar);
+   x = aVar + 1
+   printf("The value is: %d and %d\n", aVar, x);
+   return 0;// if I entered 13 the ouput is: "The value is 13 and 14"
+}
