5f85c5f1fa75709082087c48a33c85464ea6157b
Keith Miller <36780654+Keimille@users.noreply.github.com>
2018-04-22 22:55:15 -0400

    Create conditional operators.c

---
+++ conditional operators.c
@@ -0,0 +1,11 @@
+//conditional operators "?" ":"
+//condition?expression:expression for false:
+//example
+if (a>b)
+    m = a = b;
+else
+    m = a-b;
+//above example written with conditional operators
+m = a>b? a + b : a - b;
+
+nested conditional operators
