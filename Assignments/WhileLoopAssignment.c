cd38309dddf4d8cd7ac83e3f3d7e3cca69deed84
Keith Miller <36780654+Keimille@users.noreply.github.com>
2018-05-03 21:13:48 -0400

    Update WhileLoopAssignment.c

--- Assignments/WhileLoopAssignment.c
+++ Assignments/WhileLoopAssignment.c
@@ -59,11 +59,20 @@ for finding a^b.
 
 */
 
-int main(){
+#include <stdio.h>
+#include <math.h>
+
+
+int main() {
 	int quit = 0;
 	int n;  // n is the number for which you will find factorial
 	int p;  // you are going to keep the factorial of n in p
-	double base, exp; // for a^b
+	
+
+	int a;
+	int b;
+	int result;
+
 
 	int option; // for keeping the menu option
 	while(!quit){
@@ -76,21 +85,50 @@ printf(" 2. Find a^b ");
 printf(" ");
 printf(" 3. Quit ");
 		// get the response into option variable.
-scanf("%d", &option);
-	if (option == 1) {
-printf(" Which number would you like? ");
-scanf("%d", &n);
-	}
-	if (n < 0) {
-		printf(" invalid number ")
-		else if (n > 10){
-			printf( " This program can find factorials only in the range 0 - 10 ")
-		}
-		else if (n => 0 && n <= 10) {
 
+scanf("%d", &option);
+	 
+	 if (option == 2) {
+        printf(" enter a ");
+        scanf("%d", &a);
+        printf(" enter b ");
+        scanf("%d", &b);
+        result = pow(a, b);
+        printf("%d", result);
+        printf(" ");
+    } 
+    
+    else if (option == 1) {
+		printf(" Which number would you like? ");
+scanf("%d", &n);
+   
+    
+	
+	if (n < 0) {
+		printf(" invalid number ");
+	    
+	}
+		else if (n > 10) {
+			printf( " This program can find factorials only in the range 0 - 10 ");
 		}
+		else if (n >= 0 && n <= 10) {
+int fact = 1;
+for (p =1; p <= n; p++)
+	fact = fact * p;
+printf("Factorial of %d = %d\n", n, fact);
 	} 
-		
+	    }
+	else if (option == 3) {
+		printf(" YOU QUITTER!!!" );
+		quit++;
+	}
+	    
+
+return 0;	
+	    
+	}
+}
+
 		// Now. you can use if-else to check the option and do needful
 		// When option == 3, you need to do something with the variable quit, think
 		// about that.  	
