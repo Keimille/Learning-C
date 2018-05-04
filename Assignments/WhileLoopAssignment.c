0123259fc68b98867e24d99831b83c1a7b6f5b00
Keith Miller <36780654+Keimille@users.noreply.github.com>
2018-05-03 21:18:22 -0400

    Update WhileLoopAssignment.c

--- Assignments/WhileLoopAssignment.c
+++ Assignments/WhileLoopAssignment.c
@@ -1,66 +1,5 @@
 #include <stdio.h>
 #include <math.h>
-/**
-
-When the program executes it will work in the following way:
-You must accomplish all the menu options and validation checkings exactly as asked.
-
----- MENU ----
-1. Find factorial
-2. Find a^b
-3. Quit
-
-What you want to do? 1
-Enter number: -1
-Invalid number for factorial
-
----- MENU ----
-1. Find factorial
-2. Find a^b
-3. Quit
-What you want to do? 1
-Enter number: 5
-Factorial of 5 is 120
-
----- MENU ----
-1. Find factorial
-2. Find a^b
-3. Quit
-
-What you want to do? 20
-This program can find factorials only in the range 0 - 10
-
----- MENU ----
-1. Find factorial
-2. Find a^b
-3. Quit
-What you want to do? 2
-Enter a: 2
-Enter b: 5
-2^5 = 32.0
-
----- MENU ----
-1. Find factorial
-2. Find a^b
-3. Quit
-What you want to do? 5
-Invalid menu option
-
----- MENU ----
-1. Find factorial
-2. Find a^b
-3. Quit
-What you want to do? 3
-
-*** Thank you ***
-
-NOTE: You should use library function pow to find a^b, no need to write code 
-for finding a^b.
-
-*/
-
-#include <stdio.h>
-#include <math.h>
 
 
 int main() {
@@ -88,7 +27,7 @@ printf(" 3. Quit ");
 
 scanf("%d", &option);
 	 
-	 if (option == 2) {
+if (option == 2) {
         printf(" enter a ");
         scanf("%d", &a);
         printf(" enter b ");
@@ -101,12 +40,9 @@ scanf("%d", &option);
     else if (option == 1) {
 		printf(" Which number would you like? ");
 scanf("%d", &n);
-   
-    
 	
 	if (n < 0) {
-		printf(" invalid number ");
-	    
+		printf(" invalid number ");   
 	}
 		else if (n > 10) {
 			printf( " This program can find factorials only in the range 0 - 10 ");
@@ -129,17 +65,6 @@ return 0;
 	}
 }
 
-		// Now. you can use if-else to check the option and do needful
-		// When option == 3, you need to do something with the variable quit, think
-		// about that.  	
-		// When option == 1, you should read n and first check the validity,
-		// if n < 0, a particular message has to be printed, when n > 10, another 
-		// message has to be printed (see the expected output above), if n is valid
-		// write a while loop to calculate the factorial and print it, becareful about
-		// the initial value of p.
-		// for option == 2, read base and exponant then make a call to the pow function
-		// print the return value using printf. You may declare a variable to hold the 
-		// result.
 
 	}
 
