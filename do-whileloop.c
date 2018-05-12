615e8a726c6d50d6418510406bb1351976acb08b
Keith Miller <36780654+Keimille@users.noreply.github.com>
2018-05-11 21:34:47 -0400

    Update do-whileloop.c

--- do-whileloop.c
+++ do-whileloop.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 
-int main() {
-    char response1, response2;  
-    
-do {
-printf("Do you like Spam? ");
-scanf("%c", &response1);
-    if (response1 == 'Y' || response1 == 'y') { 
-        printf("I like SPAM!!! \n" "We'll have spam spam spam beak beans spam spam spam...\n" ); }
-        
-    else if (response1 == 'N' || response1 == 'n' ) 
-        printf("Who doesn't like SPAM!!!? \n"); 
-    
-printf("Would you like to try again? ");
-scanf("%c", &response2);
+int main(void)
+{
+    char response1;
+    int response2;
+    do
+    {
+        printf("Do you like Spam? ");
+        scanf("%c", &response1);
+        if (response1 == 'Y' || response1 == 'y')
+        {
+            printf("I like SPAM!!! \n"
+                   "We'll have spam spam spam beak beans spam spam spam...\n");
+        }
+
+        else if (response1 == 'N' || response1 == 'n')
+        {
+            printf("Who doesn't like SPAM!!!? \n");
+        }
+        printf("Would you like to try again?\n Enter 1 for yes or 0 for no.");
+        scanf("%d", &response2);
+    } while (response2 == 1 || response2 == 1);
+    return 0;
 }
-while (response2 == 'Y' || response2 == 'y');
-return 0;
-}
+
     
