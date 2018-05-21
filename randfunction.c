80b0d3535e2c7c85d9b86c9847f627a9d0194218
Keith Miller <36780654+Keimille@users.noreply.github.com>
2018-05-21 13:44:22 -0400

    Update randfunction.c

--- randfunction.c
+++ randfunction.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int main (void) {
+int main(void)
+{
 
-int k, i;
+    int k, i;
+    /*
 //print 12 random numbers
 //a differnt int is needed for iteration
 for (i= 1; i<=12; i++) {
 k = rand();
 printf("%d\n", k);}
-return 0;
+//srand allows for true random numbers. with 's' meening seed.
+//srand will base the random number on the number provided in parenthesis i.e srand(100)
+*/
+    long w;
+    w = time(NULL); //use the time function because time is the only thing that is constatntly changing
+    //a seed number will continue to generate the same "random" numbers
+    srand(w);
+    for (i = 1; i <= 12; i++)
+    {
+        k = rand();
+        printf("%d\n", k);
+    }
+    return 0;
 }
