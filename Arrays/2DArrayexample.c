5323a07c2a7d472d747b1559cd170c92163917ce
Keith Miller <36780654+Keimille@users.noreply.github.com>
2018-06-09 11:07:15 -0400

    Create 2DArrayexample.c

---
+++ Arrays/2DArrayexample.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (void){
+
+    int x[4][3]; //this example would be for 4 rows and 3 columns
+
+/* m x n : where m is rows and n is columns
+0 to m-1 for row index
+0 to n-1 for column index
+*/
+
+x [1][2]=5; //assigns 5 to row 1 column 2
+x[3][0]=10; //assigns 10 to row 3 column 0
+ 
+ //rows are contiguously allocated
+
+}
