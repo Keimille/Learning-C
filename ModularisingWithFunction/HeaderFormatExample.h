6e3f2515fb9c0e70518c9ac91cdc63cce9b17203
Keith Miller <36780654+Keimille@users.noreply.github.com>
2018-06-21 06:26:19 -0400

    Create HeaderFormatExample.h

---
+++ ModularisingWithFunction/HeaderFormatExample.h
@@ -0,0 +1,14 @@
+/*
+
+Enter header for .c file containing function definition(s)
+
+*/
+
+//macro command to check if header has already been included
+#indef nameOfHeaderFile_H
+#define nameOfHeaderFile_H
+
+//call function definition(s) here from function definition file
+double getSimpleInterest(double, double, double);
+
+#endif
